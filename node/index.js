573c5bc3d49a69d4ca8fac24faf4e0e85b800aef
phuongdk <phuongdkk@gmail.com>
2024-09-11 15:14:30 +0700

    First commit

---
+++ node/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(3001, () => {
+  console.log('Listening on port 3001!');
+});
+
+app.get('/', (req, res) => {});
